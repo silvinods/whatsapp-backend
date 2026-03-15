a450e751ce09149cd295b9ce6056f268f86f4ec0
unknown <silvinosoares.9ss.02@gmail.com>
2026-03-15 13:16:26 -0300

    fix qr

--- index.js
+++ index.js
@@ -85,5 +85,5 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Servidor rodando");
+    console.log("Servidor rodando nesse momento agora");
 });
